cc86e86b852f35b7a564336d71e45233c59b0bf8
Alex Vidal <alex.vidal@gmail.com>
2009-03-16 13:33:59 -0500

    Adding iambuttbot.py plugin to post all buttbot messages to twitter

---
+++ plugins/iambuttbot.py
@@ -0,0 +1,15 @@
+"""
+iambuttbot.py: avidal 2009
+posts everything buttbot says to the iambuttbot twitter account
+"""
+
+import urllib
+import hook
+
+@hook.command(hook=r'(.*)')
+def iambuttbot(bot, input)
+    if input.nick.lower() != 'buttbot': return
+    
+    status = input.inp if len(input.inp) <= 140 else input.inp[:137] + "..."
+    data = urllib.urlencode({"status": status})
+    response = urllib.urlopen("http://iambuttbot:goonsay@twitter.com/statuses/update.xml", data)
