c8df4dfb3833e9d2a4ec4a23aa47fde3991537cf
Hamled <devnull@localhost>
2010-01-16 20:12:17 -0800

    Fix to twitter replied-to feature. Currently does not function correctly.

--- plugins/goonsay.py
+++ plugins/goonsay.py
@@ -1,8 +1,1 @@
-from util import hook
-
-#Scaevolus: factormystic if you commit a re-enabled goonsay I'm going to revoke your commit access
-#@hook.command
-def goonsay(bot, input):
-    input.say(' __________    /')
-    input.say('(--[. ]-[ .]  /')
-    input.say('(_______o__)')
+../plugins_available/goonsay.py
--- plugins/profile.py
+++ plugins/profile.py
@@ -1,13 +1,1 @@
-# for crusty old rotor
-
-from util import hook
-
-
-@hook.command
-def profile(inp):
-    ".profile <username> -- links to <username>'s profile on SA"
-    if not inp:
-        return profile.__doc__
-
-    return 'http://forums.somethingawful.com/member.php?action=getinfo' + \
-            '&username=' + '+'.join(inp.split())
+../plugins_available/profile.py
