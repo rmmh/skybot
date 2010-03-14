5ce63f7f211edd098d9bc6c1538da1091aa90e90
Ryan Hitchman <hitchmanr@gmail.com>
2010-03-14 17:55:58 -0600

    let mode be set in config

--- plugins/misc.py
+++ plugins/misc.py
@@ -20,16 +20,22 @@ def invite(paraml, conn=None):
     conn.join(paraml[-1])
 
 
-#join channels when server says hello & identify bot
 @hook.event('004')
 def onjoin(paraml, conn=None):
+    # identify to services
     nickserv_password = conn.conf.get('nickserv_password', '')
     nickserv_name = conn.conf.get('nickserv_name', 'nickserv')
     nickserv_command = conn.conf.get('nickserv_command', 'IDENTIFY %s')
     if nickserv_password:
         conn.msg(nickserv_name, nickserv_command % nickserv_password)
         time.sleep(1)
-    
+
+    # set mode on self
+    mode = conn.conf.get('mode')
+    if mode:
+        conn.cmd('MODE', [conn.nick, mode])
+
+    # join channels
     for channel in conn.channels:
         conn.join(channel)
         time.sleep(1)  # don't flood JOINs
