b845bfcf25a0c4e25e6e7d1fe03debc24415c473
Ryan Hitchman <hitchmanr@gmail.com>
2010-05-10 18:02:15 -0600

    hurf (I should stop doing whitespace changes that break things)

--- plugins/sieve.py
+++ plugins/sieve.py
@@ -5,7 +5,7 @@ from util import hook
 
 @hook.sieve
 def sieve_suite(bot, input, func, kind, args):
-    if input.command == 'PRIVMSG' and
+    if input.command == 'PRIVMSG' and \
        input.nick.lower()[-3:] == 'bot' and args.get('ignorebots', True):
             return None
 
