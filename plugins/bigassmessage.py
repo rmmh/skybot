97054fe6662af0cee72f2d228df89a9b3a75c7f5
Eric Palmitesta <epalmitesta@gmail.com>
2011-02-23 03:23:13 +0800

    updating bigassmessage.py url, url params, and styles

--- plugins/bigassmessage.py
+++ plugins/bigassmessage.py
@@ -4,19 +4,20 @@ from util import hook, http
 @hook.command
 def bam(inp):
     ".bam [basic|magic|pepsi|jprdy] <message> -- creates a big ass message"
-
+    
     host = 'http://bigassmessage.com/'
-    path = 'dsx_BAM/boe.php?'
-    params = {'action': 'saveMsg', 'theStyle': 'basic', 'theMessage': inp}
-
+    path = 'BAM.php?'
+    params = {'action': 'SAVE', 'theStyle': 'basic', 'theMessage': inp}
+    
     if ' ' in inp:
         style, message = inp.split(None, 1)
-        if style in ['basic', 'magic', 'pepsi', 'jprdy']:
+        if style in ['basic', 'magic', 'heart', 'krugr']:
             params['theStyle'] = style
             params['theMessage'] = message
-
+    
     response = http.get_xml(host + path, params)
     status = response.xpath('//status/text()')[0]
+    
     if status == 'ok':
         return host + response.xpath('//msgid/text()')[0]
     else:
