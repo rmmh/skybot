2cf4476f31ce778a182712bf55d1571964d44c30
tcneyland <tcneyland@instanceoftom.com>
2011-04-11 19:09:17 -0400

    Removed unnecessary linking to vimeo screenshot

--- plugins/vimeo.py
+++ plugins/vimeo.py
@@ -6,7 +6,7 @@ video_regex = (r'(?i)http://(?:www\.)?vimeo.com/([A-Za-z0-9\-]+)')
 r'([a-zA-Z]+://|www\.)[^ ]+'
 video_request_base = "http://vimeo.com/api/v2/video/%s.json"
 
-video_info = "\x02%(title)s\x02 - length \x02%(duration)ss\x02 - \x02%(stats_number_of_likes)s\x02 likes - \x02%(stats_number_of_plays)s\x02 plays - \x02%(user_name)s\x02 on \x02%(upload_date)s\x02 - %(thumbnail_large)s "
+video_info = "\x02%(title)s\x02 - length \x02%(duration)ss\x02 - \x02%(stats_number_of_likes)s\x02 likes - \x02%(stats_number_of_plays)s\x02 plays - \x02%(user_name)s\x02 on \x02%(upload_date)s\x02"
 
 
 def viemo_video_info(video_id):
