d1a5aae7f02f46dc47b2936657cebff2d2f5c63d
Frank Crook <frank.crook@gmail.com>
2012-01-31 08:10:18 -1000

    Just some refactoring and commenting

--- plugins/quote.py
+++ plugins/quote.py
@@ -4,62 +4,86 @@ import time
 
 from util import hook
 
+def format_quote(q, num, n_quotes):
+    """Returns a formatted string of a quote"""
+    ctime, nick, msg = q
+    return "[%d/%d] %s <%s> %s" % (num, n_quotes,
+        time.strftime("%Y-%m-%d", time.gmtime(ctime)), nick, msg)
+
+def create_table_if_not_exists(db):
+    """Creates an empty quote table if one does not already exist"""
+    db.execute('''CREATE TABLE IF NOT EXISTS quote (
+                     chan, 
+                     nick, 
+                     add_nick, 
+                     msg, 
+                     time real,
+                     deleted default 0, 
+                     PRIMARY KEY (chan, nick, msg)
+                  )''')
+    db.commit()
 
 def add_quote(db, chan, nick, add_nick, msg):
-    db.execute('''insert or fail into quote (chan, nick, add_nick,
-                    msg, time) values(?,?,?,?,?)''',
-                    (chan, nick, add_nick, msg, time.time()))
-    db.commit()
+    """Adds a quote to a nick, returns message string"""
+    try:
+        db.execute('''INSERT OR FAIL INTO quote 
+                      (chan, nick, add_nick, msg, time) 
+                      VALUES(?,?,?,?,?)''',
+                   (chan, nick, add_nick, msg, time.time()))
+        db.commit()
+    except db.IntegrityError:
+        return "message already stored, doing nothing."
+    return "quote added."
 
 
 def del_quote(db, chan, nick, add_nick, msg):
-    db.execute('''update quote set deleted = 1 where
-                  chan=? and lower(nick)=lower(?) and msg=msg''')
+    """Deletes a quote from a nick"""
+    db.execute('''UPDATE quote 
+                  SET deleted = 1 
+                  WHERE chan=? 
+                  AND lower(nick)=lower(?)
+                  AND msg=msg''')
     db.commit()
 
 
 def get_quotes_by_nick(db, chan, nick):
-    return db.execute("select time, nick, msg from quote where deleted!=1 "
-            "and chan=? and lower(nick)=lower(?) order by time",
-            (chan, nick)).fetchall()
+    """Return an array of every quote from a nick"""
+    return db.execute('''SELECT time, nick, msg
+                         FROM quote
+                         WHERE deleted != 1 
+                         AND chan=? and lower(nick)=lower(?)
+                         ORDER BY time''', (chan, nick)).fetchall()
 
 
 def get_quotes_by_chan(db, chan):
-    return db.execute("select time, nick, msg from quote where deleted!=1 "
-           "and chan=? order by time", (chan,)).fetchall()
+    """Return an array of every quote from a channel"""
+    return db.execute('''SELECT time, nick, msg 
+                         FROM quote
+                         WHERE deleted!=1
+                         AND chan=? 
+                         ORDER BY time''', (chan,)).fetchall()
 
-
-def format_quote(q, num, n_quotes):
-    ctime, nick, msg = q
-    return "[%d/%d] %s <%s> %s" % (num, n_quotes,
-        time.strftime("%Y-%m-%d", time.gmtime(ctime)), nick, msg)
 
 
 @hook.command('q')
 @hook.command
 def quote(inp, nick='', chan='', db=None):
-    ".q/.quote [#chan] [nick] [#n]/.quote add <nick> <msg> -- gets " \
-        "random or [#n]th quote by <nick> or from <#chan>/adds quote"
+    """.q/.quote [#chan] [nick] [#n]/.quote add <nick> <msg> --
 
-    db.execute("create table if not exists quote"
-        "(chan, nick, add_nick, msg, time real, deleted default 0, "
-        "primary key (chan, nick, msg))")
-    db.commit()
+    gets random or [#n]th quote by <nick> or from <#chan>/adds quote
+    """
+
+    create_table_if_not_exists(db)
 
     add = re.match(r"add[^\w@]+(\S+?)>?\s+(.*)", inp, re.I)
-    retrieve = re.match(r"(\S+)(?:\s+#?(-?\d+))?$", inp)
+    retrieve_nick = re.match(r"(\S+)(?:\s+#?(-?\d+))?$", inp)
     retrieve_chan = re.match(r"(#\S+)\s+(\S+)(?:\s+#?(-?\d+))?$", inp)
 
     if add:
         quoted_nick, msg = add.groups()
-        try:
-            add_quote(db, chan, quoted_nick, nick, msg)
-            db.commit()
-        except db.IntegrityError:
-            return "message already stored, doing nothing."
-        return "quote added."
-    elif retrieve:
-        select, num = retrieve.groups()
+        return add_quote(db, chan, quoted_nick, nick, msg)
+    elif retrieve_nick:
+        select, num = retrieve_nick.groups()
 
         by_chan = False
         if select.startswith('#'):
