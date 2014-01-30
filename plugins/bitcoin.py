726bf0dd4be810aafbc884fc5bf7d48c05982a7b
Ryan Hitchman <hitchmanr@gmail.com>
2014-01-30 11:42:54 -0800

    bitcoin: use BTC-e instead of MtGox

--- plugins/bitcoin.py
+++ plugins/bitcoin.py
@@ -3,14 +3,7 @@ from util import http, hook
 
 @hook.command(autohelp=False)
 def bitcoin(inp, say=None):
-    ".bitcoin -- gets current exchange rate for bitcoins from mtgox"
-    data = http.get_json("https://data.mtgox.com/api/2/BTCUSD/money/ticker")
-    data = data['data']
-    ticker = {
-        'buy': data['buy']['display_short'],
-        'high': data['high']['display_short'],
-        'low': data['low']['display_short'],
-        'vol': data['vol']['display_short'],
-    }
-    say("Current: \x0307%(buy)s\x0f - High: \x0307%(high)s\x0f"
-        " - Low: \x0307%(low)s\x0f - Volume: %(vol)s" % ticker)
+    ".bitcoin -- gets current exchange rate for bitcoins from BTC-e"
+    data = http.get_json("https://btc-e.com/api/2/btc_usd/ticker")
+    say("BTC/USD: \x0307{buy:.0f}\x0f - High: \x0307{high}\x0f"
+        " - Low: \x0307{low}\x0f - Volume: {vol_cur:.0f}".format(**data['ticker']))
