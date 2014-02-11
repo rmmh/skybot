61da8580d000bfc0318ec8b7c79b1b497248285c
Ryan Hitchman <hitchmanr@gmail.com>
2014-02-11 12:21:06 -0800

    bitcoin: round high/low to integers

--- plugins/bitcoin.py
+++ plugins/bitcoin.py
@@ -5,5 +5,5 @@ from util import http, hook
 def bitcoin(inp, say=None):
     ".bitcoin -- gets current exchange rate for bitcoins from BTC-e"
     data = http.get_json("https://btc-e.com/api/2/btc_usd/ticker")
-    say("BTC/USD: \x0307{buy:.0f}\x0f - High: \x0307{high}\x0f"
-        " - Low: \x0307{low}\x0f - Volume: {vol_cur:.0f}".format(**data['ticker']))
+    say("USD/BTC: \x0307{buy:.0f}\x0f - High: \x0307{high:.0f}\x0f"
+            " - Low: \x0307{low:.0f}\x0f - Volume: {vol_cur:.0f}".format(**data['ticker']))
