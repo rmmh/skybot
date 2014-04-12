6706a369abedaa115a6094e713f4eb2c47b67d1b
Ryan Hitchman <hitchmanr@gmail.com>
2014-04-11 22:06:17 -0700

    allow channel/server level acl whitelist/blacklisting. fixes #104

--- plugins/sieve.py
+++ plugins/sieve.py
@@ -22,8 +22,10 @@ def sieve_suite(bot, input, func, kind, args):
     if fn and fn.group(1).lower() in disabled:
         return None
 
-    acl = bot.config.get('acls', {}).get(func.__name__)
-    if acl:
+    acls = bot.config.get('acls', {})
+    for acl in [acls.get(func.__name__), acls.get(input.chan), acls.get(input.conn.server)]:
+        if acl is None:
+            continue
         if 'deny-except' in acl:
             allowed_channels = map(unicode.lower, acl['deny-except'])
             if input.chan.lower() not in allowed_channels:
@@ -31,6 +33,12 @@ def sieve_suite(bot, input, func, kind, args):
         if 'allow-except' in acl:
             denied_channels = map(unicode.lower, acl['allow-except'])
             if input.chan.lower() in denied_channels:
+                return None
+        if 'whitelist' in acl:
+            if func.__name__ not in acl['whitelist']:
+                return None
+        if 'blacklist' in acl:
+            if func.__name__ in acl['whitelist']:
                 return None
 
     if args.get('adminonly', False):
