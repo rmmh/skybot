ab2a36260e69dac5d97dfdf8e6470cb11d6b30ec
Ryan Hitchman <hitchmanr@gmail.com>
2014-05-06 19:19:42 -0700

    bf: fix order of operations

--- plugins/bf.py
+++ plugins/bf.py
@@ -45,9 +45,9 @@ def bf(inp):
     while ip < len(program):
         c = program[ip]
         if c == '+':
-            memory[mp] = memory[mp] + 1 % 256
+            memory[mp] = (memory[mp] + 1) % 256
         elif c == '-':
-            memory[mp] = memory[mp] - 1 % 256
+            memory[mp] = (memory[mp] - 1) % 256
         elif c == '>':
             mp += 1
             if mp > rightmost:
