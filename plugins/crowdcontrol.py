1b0242e9b8d98baa2ece4f2368c29fedd507069f
craisins <heycraisins@gmail.com>
2014-05-11 18:51:26 -0400

    Updated crowdcontrol per the suggestions of rmmh in the pull request.

--- plugins/crowdcontrol.py
+++ plugins/crowdcontrol.py
@@ -5,34 +5,27 @@ import re
 import time
 from util import hook
 
-rules = []
+# Use "crowdcontrol" array in config
 # syntax
 # rule:
 #   re: RegEx. regular expression to match
 #   msg: String. message to display either with kick or as a warning
-#   kick: Boolean. True/False on if to kick user
+#   kick: Integer. 1 for True, 0 for False on if to kick user
 #   ban_length: Integer. (optional) Length of time (seconds) to ban user. (-1 to never unban, 0 to not ban, > 1 for time)
 
-# rules = [
-#     {
-#         "re": r'^data:image/(bmp|gif|jpg|jpeg|png|svg);base64,',
-#         "msg": "Please don't paste base64 images. You've been banned for 60 seconds.",
-#         "kick": True,
-#         "ban_length": 60
-#     }
-# ]
 
 @hook.regex(r'.*')
-def crowdcontrol(inp, nick='', chan='', host='', conn=None):
+def crowdcontrol(inp, nick='', chan='', host='', bot=None, conn=None):
     inp = inp.group(0)
-    for rule in rules:
-        if re.search(rule['re'], inp) is not None:
+    for rule in bot.config.get('crowdcontrol', []):
+        pattern = re.compile(rule['re'])
+        if re.search(pattern, inp) is not None:
             if 'ban_length' in rule and rule['ban_length'] != 0:
-                conn.cmd("MODE " + chan + " +b " + host)
+                conn.cmd("MODE", [chan, "+b", host])
             if rule['kick']:
-                conn.cmd("KICK " + chan + " " + nick + " :" + rule['msg'])
+                conn.cmd('KICK', [chan, nick, rule['msg']])
             if 'ban_length' in rule and rule['ban_length'] > 0:
                 time.sleep(rule['ban_length'])
-                conn.cmd("MODE " + chan + " -b " + host)
+                conn.cmd("MODE", [chan, "-b", host])
             if not rule['kick']:
                 return rule['msg']
