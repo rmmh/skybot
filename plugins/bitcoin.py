69a397d45062ea98f612960f456b177e4f46de09
Mitch Robb <olordhux@gmail.com>
2017-06-09 11:59:19 -0700

    add .ethereum (#163)

--- plugins/bitcoin.py
+++ plugins/bitcoin.py
@@ -7,3 +7,10 @@ def bitcoin(inp, say=None):
     data = http.get_json("https://btc-e.com/api/2/btc_usd/ticker")
     say("USD/BTC: \x0307{buy:.0f}\x0f - High: \x0307{high:.0f}\x0f"
             " - Low: \x0307{low:.0f}\x0f - Volume: {vol_cur:.0f}".format(**data['ticker']))
+
+@hook.command(autohelp=False)
+def ethereum(inp, say=None):
+    ".ethereum -- gets current exchange rate for ethereum from BTC-e"
+    data = http.get_json("https://btc-e.com/api/2/eth_usd/ticker")
+    say("USD/ETH: \x0307{buy:.0f}\x0f - High: \x0307{high:.0f}\x0f"
+            " - Low: \x0307{low:.0f}\x0f - Volume: {vol_cur:.0f}".format(**data['ticker']))
