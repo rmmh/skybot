6709162521936c658f6c5e9ac110c3c6ee1ac880
Ryan Hitchman <hitchmanr@gmail.com>
2018-03-23 14:13:33 -0700

    polish stock plugin

--- plugins/stock.py
+++ plugins/stock.py
@@ -1,5 +1,16 @@
+#!/usr/bin/env python
+
+import re
+
 from util import hook, http
-from urllib2 import HTTPError
+
+
+def human_price(x):
+    if x > 1e9:
+        return '{:,.2f}B'.format(x / 1e9)
+    elif x > 1e6:
+        return '{:,.2f}M'.format(x / 1e6)
+    return '{:,.0f}'.format(x)
 
 
 @hook.command
@@ -15,7 +26,7 @@ def stock(inp):
             'https://api.robinhood.com/fundamentals/{}/'.format(symbol))
         quote = http.get_json(
             'https://api.robinhood.com/quotes/{}/'.format(symbol))
-    except HTTPError:
+    except http.HTTPError:
         return '{} is not a valid stock symbol.'.format(symbol)
 
     if fundamentals['open'] is None or quote['ask_price'] is None:
@@ -28,22 +39,36 @@ def stock(inp):
     price = float(quote['last_trade_price'])
     change = price - float(quote['adjusted_previous_close'])
 
+    # Extract name as Upper Case Corp Name from description.
+    name = ''
+    m = re.match(r'^([A-Z]\S* )*', fundamentals['description'])
+    if m:
+        name = m.group(0)
+
     response = {
+        'name': name,
         'change': change,
         'percent_change': 100 * change / (price - change),
         'market_cap': float(fundamentals['market_cap']),
         'symbol': quote['symbol'],
         'price': price,
-        'color': '\x035' if change < 0 else '\x033',
+        'color': '5' if change < 0 else '3',
         'high': float(fundamentals['high']),
         'low': float(fundamentals['low']),
-        'pe_ratio': float(fundamentals['pe_ratio']),
-        'average_volume': float(fundamentals['average_volume']),
+        'average_volume': human_price(float(fundamentals['average_volume'])),
+        'market_cap': human_price(float(fundamentals['market_cap'])),
+        'pe_ratio': '',
     }
 
-    return "[{symbol}] ${price:,.2f} {color}{change:,.2f} ({percent_change:,.2f}%)\x03 :: " \
-        "High: ${high:,.2f} :: " \
-        "Low: ${low:,.2f} :: " \
-        "PE: {pe_ratio:,.2f}% :: " \
-        "Volume: ${average_volume:,.2f} :: " \
-        "MCAP: ${market_cap:,.2f}".format(**response)
+    if fundamentals.get('pe_ratio'):
+        response['pe_ratio'] = ' P/E: {:,.2f} |'.format(float(fundamentals['pe_ratio']))
+
+    return ("{name}({symbol}) ${price:,.2f} \x03{color}{change:,.2f} ({percent_change:,.2f}%)\x03 | "
+            "Day Range: ${low:,.2f} - ${high:,.2f} |{pe_ratio} "
+            "Volume: ${average_volume} | "
+            "MCAP: ${market_cap}").format(**response)
+
+if __name__ == '__main__':
+    import sys
+    for arg in sys.argv[1:]:
+        print stock(arg)
