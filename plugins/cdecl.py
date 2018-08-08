f93096e323ab10ecbd636580d8194259bcab0606
James Ward <james@notjam.es>
2018-08-08 14:23:33 -0400

    use new cdecl.org API URL
    The cdecl plugin was still pointing at the old `query.php` for
    cdecl.org.  It looks like they've since moved over to using AWS
    lambda, so they new API url is an AWS endpoint:
    
    `https://xwd733f66f.execute-api.us-west-1.amazonaws.com/prod/cdecl_backend`

--- plugins/cdecl.py
+++ plugins/cdecl.py
@@ -4,4 +4,4 @@ from util import hook, http
 @hook.command
 def cdecl(inp):
 	'''.cdecl <expr> -- translate between C declarations and English, using cdecl.org'''
-	return http.get("http://cdecl.org/query.php", q=inp)
+	return http.get("https://xwd733f66f.execute-api.us-west-1.amazonaws.com/prod/cdecl_backend", q=inp)
