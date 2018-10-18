78bf92c2922f4f19c146ef5a58905656c0667bd2
Chris Skalenda <chris@skalenda.org>
2018-10-17 20:12:38 -0600

    Use goog geocoding and darksky api for weather

--- plugins/weather.py
+++ plugins/weather.py
@@ -1,15 +1,49 @@
-"weather, thanks to wunderground"
+"weather, thanks to darksky and google geocoding"
 
 from util import hook, http
 
+GEOCODING_URL = u'https://maps.googleapis.com/maps/api/geocode/json'
+DARKSKY_URL = u'https://api.darksky.net/forecast/'
 
-@hook.api_key('wunderground')
+
+def geocode_location(api_key, loc):
+    query = '?address={}&key={}'.format(http.quote(loc), api_key)
+    url = GEOCODING_URL + query
+    try:
+        parsed_json = http.get_json(url)
+    except IOError:
+        return None
+
+    return parsed_json
+
+
+def get_weather_data(api_key, lat, long):
+    query = '{key}/{lat},{long}'.format(key=api_key, lat=lat, long=long)
+    url = DARKSKY_URL + query
+    try:
+        parsed_json = http.get_json(url)
+    except IOError:
+        return None
+
+    return parsed_json
+
+
+def f_to_c(temp_f):
+    return (temp_f - 32) * 5 / 9
+
+
+def mph_to_kph(mph):
+    return mph * 1.609
+
+
+@hook.api_key('google-geocoding', 'darksky')
 @hook.command(autohelp=False)
 def weather(inp, chan='', nick='', reply=None, db=None, api_key=None):
-    ".weather <location> [dontsave] | @<nick> -- gets weather data from " \
-        "Wunderground http://wunderground.com/weather/api"
-
-    if not api_key:
+    """
+    .weather <location> [dontsave] | @<nick> -- gets weather data from
+        darksky https://darksky.net/dev
+    """
+    if 'google-geocoding' not in api_key and 'darksky' not in api_key:
         return None
 
     # this database is used by other plugins interested in user's locations,
@@ -44,92 +78,47 @@ def weather(inp, chan='', nick='', reply=None, db=None, api_key=None):
                 return weather.__doc__
         loc = loc[0]
 
-    params = [http.quote(p.strip()) for p in loc.split(',')]
+    location = geocode_location(api_key['google-geocoding'], loc)
 
-    loc = params[0]
-    state = ''
-
-    # Try to interpret the query based on the number of commas.
-    # Two commas might be city-state  city-country, or lat-long pair
-    if len(params) == 2:
-
-        state = params[1]
-
-        # Check to see if a lat, long pair is being passed. This could be done
-        # more completely with regex, and converting from DMS to decimal
-        # degrees. This is nice and simple, however.
-        try:
-            float(loc)
-            float(state)
-
-            loc = loc + ',' + state
-            state = ''
-        except ValueError:
-            state += '/'
-
-    # Assume three commas is a city-state-country triplet. Discard the state
-    # portion because that's what the API expects
-    elif len(params) == 3:
-        loc = params[0]
-        state = params[2] + '/'
-
-    url = 'http://api.wunderground.com/api/'
-    query = '{key}/geolookup/conditions/forecast/q/{state}{loc}.json' \
-            .format(key=api_key, state=state, loc=loc)
-    url += query
-
-    try:
-        parsed_json = http.get_json(url)
-    except IOError:
-        return 'Could not get data from Wunderground'
-
-    info = {}
-    if 'current_observation' not in parsed_json:
-        resp = 'Could not find weather for {inp}. '.format(inp=inp)
-
-        # In the case of no observation, but results, print some possible
-        # location matches
-        if 'results' in parsed_json['response']:
-            resp += 'Possible matches include: '
-            results = parsed_json['response']['results']
-
-            for place in results[:6]:
-                resp += '{city}, '.format(**place)
-
-                if place['state']:
-                    resp += '{state}, '.format(**place)
-
-                if place['country_name']:
-                    resp += '{country_name}; '.format(**place)
-
-            resp = resp[:-2]
-
-        reply(resp)
+    if not location or location.get(u'status') != u'OK':
+        reply('Failed to determine location for {}'.format(inp))
         return
 
-    obs = parsed_json['current_observation']
-    sf = parsed_json['forecast']['simpleforecast']['forecastday'][0]
-    info['city'] = obs['display_location']['full']
-    info['t_f'] = obs['temp_f']
-    info['t_c'] = obs['temp_c']
-    info['weather'] = obs['weather']
-    info['h_f'] = sf['high']['fahrenheit']
-    info['h_c'] = sf['high']['celsius']
-    info['l_f'] = sf['low']['fahrenheit']
-    info['l_c'] = sf['low']['celsius']
-    info['humid'] = obs['relative_humidity']
-    info['wind'] = 'Wind: {mph}mph/{kph}kph' \
-        .format(mph=obs['wind_mph'], kph=obs['wind_kph'])
-    reply('{city}: {weather}, {t_f}F/{t_c}C'
-          '(H:{h_f}F/{h_c}C L:{l_f}F/{l_c}C)'
-          ', Humidity: {humid}, {wind}'.format(**info))
+    geo = location.get(u'results', [{}])[0].get(u'geometry', {}).get(u'location', None)
+    if not geo or u'lat' not in geo or u'lng' not in geo:
+        reply('Failed to determine location for {}'.format(inp))
+        return
 
-    lat = float(obs['display_location']['latitude'])
-    lon = float(obs['display_location']['longitude'])
+    parsed_json = get_weather_data(api_key['darksky'], geo[u'lat'], geo[u'lng'])
+    current = parsed_json.get(u'currently')
+
+    if not current:
+        reply('Failed to get weather data for {}'.format(inp))
+        return
+
+    forecast = parsed_json['daily']['data'][0]
+
+    info = {
+        u'city': location[u'results'][0][u'formatted_address'],
+        u't_f': current[u'temperature'],
+        u't_c': f_to_c(current[u'temperature']),
+        u'h_f': forecast[u'temperatureHigh'],
+        u'h_c': f_to_c(forecast[u'temperatureHigh']),
+        u'l_f': forecast[u'temperatureLow'],
+        u'l_c': f_to_c(forecast[u'temperatureLow']),
+        u'weather': current[u'summary'],
+        u'humid': int(current[u'humidity'] * 100),
+        u'wind': u'Wind: {mph:.1f}mph/{kph:.1f}kph'.format(
+            mph=current[u'windSpeed'],
+            kph=mph_to_kph(current[u'windSpeed'])),
+    }
+    reply('{city}: {weather}, {t_f:.1f}F/{t_c:.1f}C'
+          '(H:{h_f:.1f}F/{h_c:.1f}C L:{l_f:.1f}F/{l_c:.1f}C)'
+          ', Humidity: {humid}%, {wind}'.format(**info))
 
     if inp and not dontsave:
         db.execute("insert or replace into "
                    "location(chan, nick, loc, lat, lon) "
                    "values (?, ?, ?, ?, ?)",
-                   (chan, nick.lower(), inp, lat, lon))
+                   (chan, nick.lower(), inp, geo[u'lat'], geo[u'lng']))
         db.commit()
