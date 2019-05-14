9f306a01354c2576ad3b95f7e81f8285010f7f7c
Ryan Hitchman <hitchmanr@gmail.com>
2019-05-13 23:36:25 -0700

    fix .snopes

--- plugins/snopes.py
+++ plugins/snopes.py
@@ -1,34 +1,75 @@
+from __future__ import unicode_literals
+
+import json
 import re
 
 from util import hook, http
 
 
-search_url = "http://search.atomz.com/search/?sp_a=00062d45-sp00000000"
+SEARCH_URL = ('https://yfrdx308zd-dsn.algolia.net/1/indexes/wp_live_searchable_posts/query'
+              '?x-algolia-application-id=YFRDX308ZD&x-algolia-api-key=7da15c5275374261c3a4bdab2ce5d321')
 
 
 @hook.command
 def snopes(inp):
     ".snopes <topic> -- searches snopes for an urban legend about <topic>"
 
-    search_page = http.get_html(search_url, sp_q=inp, sp_c="1")
-    result_urls = search_page.xpath("//a[@target='_self']/@href")
+    params = {"params": http.urlencode({"query": inp, "hitsPerPage": 5})}
+    results = http.get_json(SEARCH_URL, post_data=json.dumps(params).encode('utf8'))['hits']
 
-    if not result_urls:
+    if not results:
         return "no matching pages found"
 
-    snopes_page = http.get_html(result_urls[0])
-    snopes_text = snopes_page.text_content()
+    post = results[0]
+    for post in results:
+        if post['post_type'] == 'fact_check':
+            return fmt(post)
 
-    claim = re.search(r"Claim: .*", snopes_text).group(0).strip()
-    status = re.search(r"Status: .*", snopes_text)
 
-    if status is not None:
-        status = status.group(0).strip()
-    else:  # new-style statuses
-        status = "Status: %s." % re.search(r"FALSE|TRUE|MIXTURE|UNDETERMINED",
-                                           snopes_text).group(0).title()
+def fmt(post):
+    permalink = post['permalink']
 
-    claim = re.sub(r"[\s\xa0]+", " ", claim)   # compress whitespace
-    status = re.sub(r"[\s\xa0]+", " ", status)
+    if 'fact_check_claim' in post:
+        claim = post['fact_check_claim']
+        if post['taxonomies'].get('fact_check_category') == ['Fake News']:
+            status = 'Fake News'
+        else:
+            status = post['taxonomies']['fact_check_rating'][0]
+    else:
+        content = post['content']
+        m = re.search(r"(?:Claim|Glurge|Legend|FACT CHECK): (.*)", content)
+        if m:
+            claim = m.group(1).strip()
+        else:
+            claim = content.split('\n')[0]
+            print("???", claim)
+        if claim == 'Claim':
+            print("!!!", content)
+        m = re.search(r"FALSE|TRUE|MIXTURE|UNDETERMINED|CORRECTLY ATTRIBUTED|(?<=Status:).*", content)
+        if m:
+            status = m.group(0)
+        else:
+            status = '???'
 
-    return "%s %s %s" % (claim, status, result_urls[0])
+    claim = re.sub(r"[\s\xa0]+", " ", http.unescape(claim)).strip()   # compress whitespace
+    status = re.sub(r"[\s\xa0]+", " ", http.unescape(status)).title().strip()
+
+    if len(claim) > 300:
+        claim = claim[:300] + '...'
+
+    return "Claim: {0} Status: {1} {2}".format(claim, status, permalink)
+
+
+if __name__ == '__main__':
+    a = http.get_json(SEARCH_URL, post_data=json.dumps({"params": http.urlencode({"query": "people", "hitsPerPage": 1000})}).encode('utf8'))
+    print(len(a['hits']))
+    try:
+        for x in a['hits']:
+            if x['post_type'] == 'fact_check':
+                f = fmt(x)
+                print(f)
+                assert len(f) < 400
+    except AttributeError:
+        print(x['permalink'])
+        print(x['content'])
+        raise
