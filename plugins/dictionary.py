2b5c567b521b21dfb845e2158ada57e53a3eee74
Ell <alan@cattes.us>
2021-04-24 15:43:10 -0500

    Fix ety, run black

--- plugins/dictionary.py
+++ plugins/dictionary.py
@@ -5,44 +5,46 @@ import re
 from util import hook, http
 
 
-@hook.command('u')
+@hook.command("u")
 @hook.command
 def urban(inp):
-    '''.u/.urban <phrase> -- looks up <phrase> on urbandictionary.com'''
+    """.u/.urban <phrase> -- looks up <phrase> on urbandictionary.com"""
 
-    page = http.get_json('https://api.urbandictionary.com/v0/define', term=inp)
-    defs = page['list']
+    page = http.get_json("https://api.urbandictionary.com/v0/define", term=inp)
+    defs = page["list"]
 
     if not defs:
-        return 'not found.'
+        return "not found."
 
-    out = defs[0]['word'] + ': ' + defs[0]['definition'].replace('\r\n', ' ')
+    out = defs[0]["word"] + ": " + defs[0]["definition"].replace("\r\n", " ")
 
     if len(out) > 400:
-        out = out[:out.rfind(' ', 0, 400)] + '...'
+        out = out[: out.rfind(" ", 0, 400)] + "..."
 
     return out
 
 
 # define plugin by GhettoWizard & Scaevolus
-@hook.command('dictionary')
+@hook.command("dictionary")
 @hook.command
 def define(inp):
     ".define/.dictionary <word> -- fetches definition of <word>"
 
-    url = 'http://ninjawords.com/'
+    url = "http://ninjawords.com/"
 
     h = http.get_html(url + http.quote_plus(inp))
 
-    definition = h.xpath('//dd[@class="article"] | '
-                         '//div[@class="definition"] |'
-                         '//div[@class="example"]')
+    definition = h.xpath(
+        '//dd[@class="article"] | '
+        '//div[@class="definition"] |'
+        '//div[@class="example"]'
+    )
 
     if not definition:
-        return 'No results for ' + inp
+        return "No results for " + inp
 
     def format_output(show_examples):
-        result = '%s: ' % h.xpath('//dt[@class="title-word"]/a/text()')[0]
+        result = "%s: " % h.xpath('//dt[@class="title-word"]/a/text()')[0]
 
         correction = h.xpath('//span[@class="correct-word"]/text()')
         if correction:
@@ -50,28 +52,29 @@ def define(inp):
 
         sections = []
         for section in definition:
-            if section.attrib['class'] == 'article':
-                sections += [[section.text_content() + ': ']]
-            elif section.attrib['class'] == 'example':
+            if section.attrib["class"] == "article":
+                sections += [[section.text_content() + ": "]]
+            elif section.attrib["class"] == "example":
                 if show_examples:
-                    sections[-1][-1] += ' ' + section.text_content()
+                    sections[-1][-1] += " " + section.text_content()
             else:
                 sections[-1] += [section.text_content()]
 
         for article in sections:
             result += article[0]
             if len(article) > 2:
-                result += ' '.join('%d. %s' % (n + 1, section)
-                                   for n, section in enumerate(article[1:]))
+                result += " ".join(
+                    "%d. %s" % (n + 1, section) for n, section in enumerate(article[1:])
+                )
             else:
-                result += article[1] + ' '
+                result += article[1] + " "
 
         synonyms = h.xpath('//dd[@class="synonyms"]')
         if synonyms:
             result += synonyms[0].text_content()
 
-        result = re.sub(r'\s+', ' ', result)
-        result = re.sub('\xb0', '', result)
+        result = re.sub(r"\s+", " ", result)
+        result = re.sub("\xb0", "", result)
         return result
 
     result = format_output(True)
@@ -79,28 +82,27 @@ def define(inp):
         result = format_output(False)
 
     if len(result) > 450:
-        result = result[:result.rfind(' ', 0, 450)]
-        result = re.sub(r'[^A-Za-z]+\.?$', '', result) + ' ...'
+        result = result[: result.rfind(" ", 0, 450)]
+        result = re.sub(r"[^A-Za-z]+\.?$", "", result) + " ..."
 
     return result
 
 
-@hook.command('e')
+@hook.command("e")
 @hook.command
 def etymology(inp):
     ".e/.etymology <word> -- Retrieves the etymology of chosen word"
 
-    h = http.get_html('http://www.etymonline.com/search', q=inp)
+    h = http.get_html("https://www.etymonline.com/search", q=inp)
 
-    etym = h.xpath('//div[contains(@class, "word")]/object')
+    etym = h.xpath("string(//section[1])")
 
     if not etym:
-        return 'No etymology found for ' + inp
+        return "No etymology found for " + inp
 
-    etym = ' '.join(sum((e.text_content().split() for e in etym[0]), []))
-    etym = etym.replace(inp, '\x02%s\x02' % inp)
+    etym = etym.replace(inp, "\x02%s\x02" % inp)
 
     if len(etym) > 400:
-        etym = etym[:etym.rfind(' ', 0, 400)] + ' ...'
+        etym = etym[: etym.rfind(" ", 0, 400)] + " ..."
 
     return etym
