e061a2f71c23410b18d09fec36fcbab013776973
Lindsey B <hazardous.narayan@gmail.com>
2021-06-25 12:34:11 -0400

    Elgruntox this up

--- plugins/tarot.py
+++ plugins/tarot.py
@@ -1,26 +1,30 @@
 """
 🔮 Spooky fortunes and assistance for witches
 """
+from future.standard_library import hooks
+
+with hooks():
+    from urllib.error import HTTPError
+    from urllib.parse import quote
+
 from util import hook, http
-from urllib.error import HTTPError
+
 
 @hook.command
 def tarot(inp):
     ".tarot <cardname> -- finds a card by name"
+    search = quote(inp)
 
     try:
-        card = http.get_json("https://tarot-api.com/find/{search}", search=inp)
+        card = http.get_json(f"https://tarot-api.com/find/{search}")
     except HTTPError:
         return "the spirits are displeased."
 
-
     return card["name"] + ": " + ", ".join(card["keywords"])
 
-@hook.command(autohelp=False)
-def fortune(inp):
-    """
-    .fortune -- returns one random card and it's fortune
-    """
+
+def fortune():
+    ".fortune -- returns one random card and it's fortune"
 
     try:
         cards = http.get_json("https://tarot-api.com/draw/1")
@@ -30,3 +34,8 @@ def fortune(inp):
     card = cards[0]
 
     return card["name"] + ": " + ", ".join(card["keywords"])
+
+
+if __name__ == '__main__':
+    result = tarot("ten of swords")
+    print(result)
