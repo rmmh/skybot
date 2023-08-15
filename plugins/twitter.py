3bfaad863a833a25b145b094a803f742d3aec1b8
Ryan Hitchman <hitchmanr@gmail.com>
2023-08-14 20:38:23 -0600

    fix twitter by adding new token query param

--- plugins/twitter.py
+++ plugins/twitter.py
@@ -1,11 +1,50 @@
 from __future__ import unicode_literals
 
+import math
 import random
 import re
+import string
 from time import strptime, strftime
 from urllib.parse import quote
 
 from util import hook, http
+
+
+b36_alphabet = string.digits + string.ascii_lowercase
+
+def base36(x):
+    frac, x = math.modf(x)
+    r = []
+    x = int(x)
+    f = []
+    # fractional part translated from V8 DoubleToRadixCString
+    delta = max(0.5 * (math.nextafter(x, x+1) - x), math.nextafter(0, 1))
+    while frac >= delta:
+        frac *= 36
+        delta *= 36
+        d = int(frac)
+        f.append(d)
+        frac -= d
+        if frac > .5 or frac == .5 and d & 1:
+            if frac + delta > 1:
+                i = len(f)
+                while True:
+                    i -= 1
+                    if i == -1:
+                        x += 1
+                        break
+                    if f[i] + 1 < 36:
+                        f[i] += 1
+                        break
+    f = [b36_alphabet[x] for x in f]
+    while x:
+        x, rem = divmod(x, 36)
+        r.append(b36_alphabet[rem])
+
+    if f:
+        return ''.join(r[::-1]) + '.' + ''.join(f)
+    else:
+        return ''.join(r[::-1])
 
 
 @hook.command
@@ -13,12 +52,14 @@ def twitter(inp):
     ".twitter <id> -- get tweet <id>"
 
     if re.match(r"^\d+$", inp):
-        request_url = "https://cdn.syndication.twimg.com/tweet-result?id=%s&lang=en" % inp
+        x = int(inp)/1e15*math.pi
+        token = re.sub(r'0+|\.', '', base36(x))
+        request_url = "https://cdn.syndication.twimg.com/tweet-result?lang=en"
     else:
         return 'error: can only get tweets by id'
 
     try:
-        tweet = http.get_json(request_url)
+        tweet = http.get_json(request_url, id=inp, token=token)
     except http.HTTPError as e:
         errors = {
             400: "bad request (ratelimited?)",
@@ -36,8 +77,6 @@ def twitter(inp):
             return "error: " + errors[e.code]
         return "error: unknown %s" % e.code
 
-    print(tweet)
-
     if "retweeted_status" in tweet:
         rt = tweet["retweeted_status"]
         rt_text = http.unescape(rt["full_text"]).replace("\n", " ")
@@ -49,7 +88,6 @@ def twitter(inp):
             if len(new_text) < 350:
                 text = new_text
         for url in tweet.get('mediaDetails', []) + tweet.get('entities', {}).get('media', []):
-            print(url)
             if url.get('type') in ('video', 'animated_gif'):
                 try:
                     media_url = max(url['video_info']['variants'], key=lambda x: x.get('bitrate', 0))['url']
